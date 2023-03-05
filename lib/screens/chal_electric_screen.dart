3844104c8c4a77a2d6bd63e833c8b91ef9b15bba
timotheekim10 <timotheekim10@gmail.com>
2023-03-06 00:36:02 +0900

    Design the User Interface: Base

--- lib/screens/chal_electric_screen.dart
+++ lib/screens/chal_electric_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eduwithu/question_list.dart';
 import 'package:flutter/material.dart';
 
 class ChalElectricScreen extends StatelessWidget {
@@ -18,6 +19,118 @@ class ChalElectricScreen extends StatelessWidget {
         ),
       ),
       backgroundColor: const Color(0xFFE5F7F1),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 100),
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 20,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color(0xFF94E1D4)),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(questionList[0]["questionText"]),
+            ),
+            const SizedBox(height: 150),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(questionList[0]["answers"][0]["text"]),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Color(0xFF94E1D4);
+                  } else {
+                    return Colors.white;
+                  }
+                }),
+                textStyle: MaterialStateProperty.resolveWith(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return TextStyle(fontSize: 16);
+                    } else {
+                      return TextStyle(fontSize: 10);
+                    }
+                  },
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(questionList[0]["answers"][1]["text"]),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Color(0xFF94E1D4);
+                  } else {
+                    return Colors.white;
+                  }
+                }),
+                textStyle: MaterialStateProperty.resolveWith(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return TextStyle(fontSize: 16);
+                    } else {
+                      return TextStyle(fontSize: 10);
+                    }
+                  },
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(questionList[0]["answers"][2]["text"]),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Color(0xFF94E1D4);
+                  } else {
+                    return Colors.white;
+                  }
+                }),
+                textStyle: MaterialStateProperty.resolveWith(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return TextStyle(fontSize: 16);
+                    } else {
+                      return TextStyle(fontSize: 10);
+                    }
+                  },
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(questionList[0]["answers"][3]["text"]),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Color(0xFF94E1D4);
+                  } else {
+                    return Colors.white;
+                  }
+                }),
+                textStyle: MaterialStateProperty.resolveWith(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return TextStyle(fontSize: 16);
+                    } else {
+                      return TextStyle(fontSize: 10);
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
