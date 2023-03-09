20a20cd8057a9ffa20d9f2ba582ef91bcc0eefe7
timotheekim10 <timotheekim10@gmail.com>
2023-03-10 00:02:55 +0900

    Make Quiz Function

--- lib/screens/chal_electric_screen.dart
+++ lib/screens/chal_electric_screen.dart
@@ -1,8 +1,33 @@
 import 'package:eduwithu/question_list.dart';
+import 'package:eduwithu/screens/quiz_electric_screen.dart';
+import 'package:eduwithu/screens/result_electric_screen.dart';
 import 'package:flutter/material.dart';
 
-class ChalElectricScreen extends StatelessWidget {
+class ChalElectricScreen extends StatefulWidget {
   const ChalElectricScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChalElectricScreen> createState() => _ChalElectricScreenState();
+}
+
+class _ChalElectricScreenState extends State<ChalElectricScreen> {
+  int questionIndex = 0;
+  int totalScore = 0;
+
+  void answerPressed(int score) {
+    setState(() {
+      questionIndex++;
+    });
+    totalScore += score;
+    print(totalScore);
+  }
+
+  void resetQuiz() {
+    setState(() {
+      questionIndex = 0;
+    });
+    totalScore = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,118 +44,15 @@ class ChalElectricScreen extends StatelessWidget {
         ),
       ),
       backgroundColor: const Color(0xFFE5F7F1),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 100),
-            Container(
-              padding: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 20,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Color(0xFF94E1D4)),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(questionList[0]["questionText"]),
+      body: (questionIndex < questionList.length)
+          ? QuizElectricScreen(
+              answerPressed: answerPressed,
+              questionIndex: questionIndex,
+            )
+          : ResultElectricScreen(
+              totalScore: totalScore,
+              resetQuiz: resetQuiz,
             ),
-            const SizedBox(height: 150),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(questionList[0]["answers"][0]["text"]),
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-                backgroundColor: MaterialStateProperty.resolveWith((states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Color(0xFF94E1D4);
-                  } else {
-                    return Colors.white;
-                  }
-                }),
-                textStyle: MaterialStateProperty.resolveWith(
-                  (states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return TextStyle(fontSize: 16);
-                    } else {
-                      return TextStyle(fontSize: 10);
-                    }
-                  },
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(questionList[0]["answers"][1]["text"]),
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-                backgroundColor: MaterialStateProperty.resolveWith((states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Color(0xFF94E1D4);
-                  } else {
-                    return Colors.white;
-                  }
-                }),
-                textStyle: MaterialStateProperty.resolveWith(
-                  (states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return TextStyle(fontSize: 16);
-                    } else {
-                      return TextStyle(fontSize: 10);
-                    }
-                  },
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(questionList[0]["answers"][2]["text"]),
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-                backgroundColor: MaterialStateProperty.resolveWith((states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Color(0xFF94E1D4);
-                  } else {
-                    return Colors.white;
-                  }
-                }),
-                textStyle: MaterialStateProperty.resolveWith(
-                  (states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return TextStyle(fontSize: 16);
-                    } else {
-                      return TextStyle(fontSize: 10);
-                    }
-                  },
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(questionList[0]["answers"][3]["text"]),
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-                backgroundColor: MaterialStateProperty.resolveWith((states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Color(0xFF94E1D4);
-                  } else {
-                    return Colors.white;
-                  }
-                }),
-                textStyle: MaterialStateProperty.resolveWith(
-                  (states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return TextStyle(fontSize: 16);
-                    } else {
-                      return TextStyle(fontSize: 10);
-                    }
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
