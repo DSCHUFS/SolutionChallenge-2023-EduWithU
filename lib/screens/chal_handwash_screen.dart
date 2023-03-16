4e425840f7c52bc58ee71d06347aa1ebd04ca93a
KIMEUNBI <hufseunbi@gmail.com>
2023-03-16 22:05:05 +0900

    chall-with

--- lib/screens/chal_handwash_screen.dart
+++ lib/screens/chal_handwash_screen.dart
@@ -1,7 +1,76 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:video_player/video_player.dart';
 
-class ChalHandwashScreen extends StatelessWidget {
-  const ChalHandwashScreen({Key? key}) : super(key: key);
+import '../main.dart';
+
+void main() => runApp(ChalHandwashScreenApp());
+
+class ChalHandwashScreenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title',
+      home: ChalHandwashScreen(),
+    );
+  }
+}
+
+class ChalHandwashScreen extends StatefulWidget {
+  ChalHandwashScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChalHandwashScreenState createState() => _ChalHandwashScreenState();
+}
+
+class _ChalHandwashScreenState extends State<ChalHandwashScreen> with SingleTickerProviderStateMixin {
+  late VideoPlayerController _controller;
+  late AnimationController _timer;
+  late Animation<Color?> _colorTween;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  get floatingActionButton => null;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    _timer = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 7)
+    )..addListener(() {
+      setState(() {});
+    });
+
+    _timer.forward();
+
+    _timer.addListener(() {
+      // setState를 주지 않으면 애니메이션이 적용되지 않습니다.
+      setState(() {});
+      print(_timer.value);
+    });
+
+    _timer.repeat(reverse: true);
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    _controller.setLooping(true);
+
+    _colorTween = _timer.drive(ColorTween(begin: Colors.yellow, end: Colors.blue));
+
+
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _timer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +86,80 @@ class ChalHandwashScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: SingleChildScrollView(
+          child: Column(children: [
+            const SizedBox(height: 50),
+        SizedBox(
+          width: double.infinity,
+          height: 500,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Container(
+                child: FutureBuilder(
+                  future: _initializeVideoPlayerFuture,
+                  builder:  (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return ClipRRect(
+                          child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                    width: _controller.value.size?.width ?? 0,
+                    height: _controller.value.size?.height ?? 0,
+                          child: Stack( children: [ VideoPlayer(_controller), Center(
+                                child: FloatingActionButton(
+                                  backgroundColor: Colors.transparent,
+                                  hoverColor: const Color(0xFF94E1D4),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (_controller.value.isPlaying) {
+                                        _controller.pause();
+                                      } else {
+                                        _controller.play();
+                                      }
+                                    });
+                                  },
+                                  child: Icon(
+                                    _controller.value.isPlaying
+                                        ? Icons.pause
+                                        : Icons.play_arrow,
+                                  ),
+                                ))]),
+                        ),
+                      ));
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                )),
+            ],
+          ),
+        ),
+            Container(  height: 30,
+                child: VideoProgressIndicator(
+              _controller,
+              allowScrubbing: true,
+              colors: VideoProgressColors(
+                  backgroundColor: Colors.white,
+                  bufferedColor: Colors.white,
+                  playedColor: const Color(0xFF94E1D4)),
+            )),
+            const SizedBox(height: 30),
+            Center(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xFF94E1D4),
+                    onPrimary: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Home()));
+                  },
+                  child: Text('Next'),
+                )),
+          ])),
       backgroundColor: const Color(0xFFE5F7F1),
     );
   }
--- lib/screens/edu_sanitary_screen.dart
+++ lib/screens/edu_sanitary_screen.dart
@@ -32,16 +32,12 @@ class _EduSanitaryScreenState extends State<EduSanitaryScreen> {
 
   @override
   void initState() {
-    // VideoPlayerController를 저장하기 위한 변수를 만듭니다. VideoPlayerController는
-    // asset, 파일, 인터넷 등의 영상들을 제어하기 위해 다양한 생성자를 제공합니다.
     _controller = VideoPlayerController.network(
       'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
     );
 
-    // 컨트롤러를 초기화하고 추후 사용하기 위해 Future를 변수에 할당합니다.
     _initializeVideoPlayerFuture = _controller.initialize();
 
-    // 비디오를 반복 재생하기 위해 컨트롤러를 사용합니다.
     _controller.setLooping(true);
 
     super.initState();
@@ -49,7 +45,6 @@ class _EduSanitaryScreenState extends State<EduSanitaryScreen> {
 
   @override
   void dispose() {
-    // 자원을 반환하기 위해 VideoPlayerController를 dispose 시키세요.
     _controller.dispose();
     super.dispose();
   }
@@ -68,16 +63,12 @@ class _EduSanitaryScreenState extends State<EduSanitaryScreen> {
           ),
         ),
       ),
-      // VideoPlayerController가 초기화를 진행하는 동안 로딩 스피너를 보여주기 위해
-      // FutureBuilder를 사용합니다.
       body: SingleChildScrollView(
           child: Column(children: [
         FutureBuilder(
           future: _initializeVideoPlayerFuture,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              // 만약 VideoPlayerController 초기화가 끝나면, 제공된 데이터를 사용하여
-              // VideoPlayer의 종횡비를 제한하세요.
               return AspectRatio(
                 aspectRatio: _controller.value.aspectRatio,
                 child: ClipRRect(
@@ -88,14 +79,10 @@ class _EduSanitaryScreenState extends State<EduSanitaryScreen> {
                       backgroundColor: Colors.transparent,
                       hoverColor: const Color(0xFF94E1D4),
                       onPressed: () {
-                        // 재생/일시 중지 기능을 `setState` 호출로 감쌉니다. 이렇게 함으로써 올바른 아이콘이
-                        // 보여집니다.
                         setState(() {
-                          // 영상이 재생 중이라면, 일시 중지 시킵니다.
                           if (_controller.value.isPlaying) {
                             _controller.pause();
                           } else {
-                            // 만약 영상이 일시 중지 상태였다면, 재생합니다.
                             _controller.play();
                           }
                         });
@@ -110,8 +97,6 @@ class _EduSanitaryScreenState extends State<EduSanitaryScreen> {
                 ),
               );
             } else {
-              // 만약 VideoPlayerController가 여전히 초기화 중이라면,
-              // 로딩 스피너를 보여줍니다.
               return Center(child: CircularProgressIndicator());
             }
           },
