9287b0ce776a1eb868e37a729ceac95f822c8665
Shin Hyeongcheol <hcshin0717@naver.com>
2023-03-20 18:42:30 +0900

    Firebase Test & Instance

--- lib/FirebaseTestScreen.dart
+++ lib/FirebaseTestScreen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eduwithu/Data/Edudata_Read.dart';
 import 'package:eduwithu/screens/home_screen.dart';
 import 'package:eduwithu/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
@@ -12,14 +14,37 @@ class TestF extends StatefulWidget {
 }
 
 class _TestFState extends State<TestF> {
+  final db = FirebaseFirestore.instance.collection('Test');
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Edu with U',
       home: Scaffold(
-        body: Text('abc'),
+        body: StreamBuilder(
+          stream: db.snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                  streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index){
+                  final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(documentSnapshot['name']),
+                      subtitle: Text(documentSnapshot['test']),
+                    ),
+                  );
+                },
+              );
+            }
+            return Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
-    );;
+    );
+    ;
   }
 }
